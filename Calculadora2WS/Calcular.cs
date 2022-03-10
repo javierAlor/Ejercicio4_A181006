8bb5e77d25cce2dff825f7a6cab8b7618faffac6
Javier Alor <javier.alor.dev@gmail.com>
2022-03-10 13:55:27 -0600

    Add project files.

---
+++ Calculadora2WS/Calcular.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculadora2WS
+{
+    public class Calcular
+    {
+        public decimal lado1 { get; set; }
+        public decimal lado2 { get; set; }
+
+        public decimal a { get; set; }
+        public decimal b { get; set; }
+        public double r { get; set; }
+
+        public decimal cuadrado(decimal l1, decimal l2)
+        {   
+            lado1=l1;
+            lado2=l2;
+            return lado1* lado2;
+        }
+        public decimal triangulo(decimal b, decimal a)
+        {
+            return (b * a) / 2;
+        }
+        public double circulo(double r)
+        {
+            return 3.1416 *(r*r);
+        }
+
+
+    }
+}
---
+++ Calculadora2WS/WebService1.asmx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Calculadora2WS
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public decimal cuadrado(decimal lado1, decimal lado2) {
+            Calcular c = new Calcular();
+            return c.cuadrado(lado1, lado2);
+        }
+        [WebMethod]
+        public decimal triangulo(decimal b, decimal a)
+        {
+            Calcular c = new Calcular();
+            return c.triangulo(a , b);
+        }
+        [WebMethod]
+        public double circulo(double radio)
+        {
+            Calcular c = new Calcular();
+            return c.circulo(radio);
+        }
+
+
+
+    }
+}
